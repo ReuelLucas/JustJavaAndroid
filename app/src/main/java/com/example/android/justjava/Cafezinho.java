56103e7950c4d8173ab8352f2523e1a7adcb73bf
Mazinhu <matheuzmarinho@gmail.com>
2016-09-11 23:53:47 -0300

    Primeira versão

---
+++ app/src/main/java/com/example/android/justjava/Cafezinho.java
@@ -0,0 +1,21 @@
+package com.example.android.justjava;
+
+/**
+ * Created by Hellow on 10/09/2016.
+ */
+
+/* Class Cafe */
+public class Cafezinho {
+    private int quantidade;
+
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+
+}
