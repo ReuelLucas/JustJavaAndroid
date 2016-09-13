e10a6270be62de823a22230d7a17674bf3d8e327
Mazinhu <matheuzmarinho@gmail.com>
2016-09-12 22:39:29 -0300

    Terceira Versão

---
+++ app/src/main/java/com/example/android/justjava/Produto.java
@@ -0,0 +1,17 @@
+package com.example.android.justjava;
+
+/**
+ * Created by Hellow on 12/09/2016.
+ */
+public class Produto {
+    public String name;
+    public Boolean status;
+
+    public Produto() {
+    }
+
+    public Produto(String name, Boolean status) {
+        this.name = name;
+        this.status = status;
+    }
+}
